2f50054ff5f7e36581358dbac148e90e4323dbc2
Dante Escamilla <64595556+descamillaa98@users.noreply.github.com>
2026-09-22 13:21:17 -0600

    Se ajusta main para que funcione NavBar adecuadamente

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Selección de elementos del DOM
+  const header = document.getElementById('header');
+  const navToggle = document.getElementById('nav-toggle');
+  const navMenu = document.getElementById('nav-menu');
+  const toggleIcon = document.getElementById('toggle-icon');
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+
+    //Cambiar entre íconos menú hamburguesa y tache para cerrarlo
+    if (navMenu.classList.contains('active')) {
+      toggleIcon.classList.remove('fa-bars');
+      toggleIcon.classList.add('fa-xmark');
+    } else {
+      toggleIcon.classList.remove('fa-xmark');
+      toggleIcon.classList.add('fa-bars');
+    }
+  });
+
+  //Cerrar el menú anvorgesa al hacer clic en cualquier enlace
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      navMenu.classList.remove('active');
+      toggleIcon.classList.remove('fa-xmark');
+      toggleIcon.classList.add('fa-bars');
+    });
+  });
+});
 
